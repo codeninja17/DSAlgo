4571297c0e135062b261fcdcc99a0f6cd1e12e38
Ankit Kumar <ankitkr09@gmail.com>
2022-01-21 20:47:42 +0530

    Recursion and backtracking

--- questions/leetcode/MaximumSubArray.java
+++ questions/leetcode/MaximumSubArray.java
@@ -10,15 +10,30 @@ public class MaximumSubArray     {
             return arr[l];
         }
         int mid = l + (h-l)/2;
-        int leftSum = maxSum(arr,l,mid);
-        int rightSum = maxSum(arr,mid+1,h);
-        System.out.println(l + " " + mid + " "+h);
-        return 1;
+
+        int leftSum = 0; int maxLeftSum = Integer.MIN_VALUE;
+        for(int i = mid;i >= l;i--){
+            leftSum += arr[i];
+            if(leftSum > maxLeftSum){
+                maxLeftSum = leftSum;
+            }
+        }
+
+        int rightSum = 0 ; int maxRightSum = Integer.MIN_VALUE;
+        for (int j = mid+1;j<=h;j++){
+            rightSum += arr[j];
+            if(rightSum > maxRightSum){
+                maxRightSum = rightSum;
+            }
+        }
+        int maxRightLeft = Math.max( maxSum(arr,l,mid) ,  maxSum(arr,mid+1,h));
+        int max = Math.max(maxRightLeft , maxLeftSum + maxRightSum);
+        return max;
     }
 
     public static void main(String[] args) {
         int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
-        maxSum(arr,0,arr.length-1);
+        System.out.println(maxSum(arr,0,arr.length-1));
     }
 
 }
